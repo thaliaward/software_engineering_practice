130b7f50b272a88615f0458662a9af4c96277fb7
thaliaward <thalia.ward@yahoo.com>
2025-10-07 01:05:00 -0400

    more practice for rectangle lab

--- rectangle_lab/calculate_area.js
+++ rectangle_lab/calculate_area.js
@@ -7,3 +7,15 @@ function calculateArea() {
     let area = length * width;
     document.getElementById("result").innerText = `The area of the rectangle is ${area}.`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateGroceries() {
+    grocery1 = parseFloat(document.getElementById("grocery1").value);
+    grocery2 = parseFloat(document.getElementById("grocery2").value);
+    grocery3 = parseFloat(document.getElementById("grocery3").value);
+    let total = grocery1 + grocery2 + grocery3;
+    document.getElementById("groceryResult").innerText = `The total amount is: $${total}`;
+}
