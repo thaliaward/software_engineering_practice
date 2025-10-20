11bcc58b049ae9d7f3f355d99c20bcf73aefc49f
thaliaward <thalia.ward@yahoo.com>
2025-10-20 19:54:18 -0400

    practice for todo list lab

--- todolist_lab/todo_list.js
+++ todolist_lab/todo_list.js
@@ -3,6 +3,7 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllTasks");
 
 let tasks = [];
 
@@ -36,7 +37,13 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener('click', clearAllTasks);
 
 displayTasks();
